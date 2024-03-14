87a9775ecb0b93941898848eb986a6a7e4c3d4a2
Bruno Horta <bruno.horta@avanceesoftware.com>
2024-03-14 17:30:36 +0000

    registry led

--- idf-registry/main/main.cpp
+++ idf-registry/main/main.cpp
@@ -1,35 +1,42 @@
 #include "main.hpp"
-// INCLUDE LOG LIB
-#include <esp_log.h>
-// DEFINE TAGS
-#define TAG "RANDOM"
-#include "esp_random.h"
-#include "math.h"
-
-esp_err_t General::Core::setup()
-{
-    ESP_LOGI(TAG, "SETUP");
-    return ESP_OK;
-}
-
-void General::Core::run()
-{
-    ESP_LOGI(TAG, "LOOP");
-    int random = esp_random();
-    ESP_LOGI(TAG, "RANDOM NUMBER %d", random);
-    int positiveNumber = abs(random);
-    ESP_LOGI(TAG, "POSITIVE NUMBER %d", positiveNumber);
-    int limitedNumber = (positiveNumber % 6) + 1;
-    ESP_LOGI(TAG, "LIMITED NUMBER %d", limitedNumber);
-    vTaskDelay(pdMS_TO_TICKS(5000));
-}
+#include "led_strip.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#define BLINK_GPIO 38
 
 extern "C" void app_main()
 {
-    General::Core core;
-    core.setup();
+    led_strip_handle_t led_strip;
+
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,             // The GPIO that connected to the LED strip's data line
+        .max_leds = 1,                            // The number of LEDs in the strip,
+        .led_pixel_format = LED_PIXEL_FORMAT_GRB, // Pixel format of your LED strip
+        .led_model = LED_MODEL_WS2812,            // LED strip model
+        .flags = {.invert_out = false},           // whether to invert the output signal (useful when your hardware has a level inverter)
+    };
+
+    led_strip_spi_config_t spi_config;
+    spi_config.spi_bus = SPI2_HOST;
+    spi_config.flags.with_dma = true;         // Using DMA can improve performance and help drive more LEDs
+    spi_config.clk_src = SPI_CLK_SRC_DEFAULT; // different clock source can lead to different power consumption
+
+    // SPI bus ID
+    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
+    led_strip_clear(led_strip);
     while (true)
     {
-        core.run();
+        led_strip_set_pixel(led_strip, 0, 50, 0, 0);
+        led_strip_refresh(led_strip);
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        led_strip_set_pixel(led_strip, 0, 0, 50, 0);
+        led_strip_refresh(led_strip);
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        led_strip_set_pixel(led_strip, 0, 0, 0, 50);
+        led_strip_refresh(led_strip);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
